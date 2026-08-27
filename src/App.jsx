c90c00f486a5ab258e091734660a3827235f2a6a
Sounak Banerjee <sounakbanerjee28@gmail.com>
2026-08-27 20:23:17 +0530

    Updated and Fixed Some Issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import Navbar from "./components/Navbar";
-
 import Hero from "./components/Hero";
-
-import About from "./components/About"
-
-import Skills from "./components.Skills"
-
+import About from "./components/About";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-
 import Experience from "./components/Experience";
-
 import Education from "./components/Education";
-
 import Contact from "./components/Contact";
-
 import Footer from "./components/Footer";
 
-function App()
-{
-    return 
-    {
+function App() {
+    return (
         <>
             <Navbar />
             <Hero />
@@ -31,5 +21,7 @@ function App()
             <Contact />
             <Footer />
         </>
-    };
+    );
 }
+
+export default App;
